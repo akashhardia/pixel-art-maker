0a00efcb0e85cabfe6e4f0a4929b8277714a000f
Akash Hardia <aakashhardiya@gmail.com>
2018-09-17 16:21:11 +0000

    major- Jquery removed improved performance with less listeners and only vanilla Javascript. JQuery removed.

--- designs.js
+++ designs.js
@@ -1,36 +1,35 @@
-//variables to get canvas element,height,width
-const c = document.getElementById('pixelCanvas');
-let temph = $("#inputHeight");
-let tempw = $("#inputWidth");
+let canvas          = document.getElementById('pixelCanvas'),
+    submitButton    = document.getElementById('input_submit');
 
-//An event listener that comes into action when Submit button is clicked .
-$('#input_submit').click(function(e) {
-    e.preventDefault();
-    makeGrid();
+submitButton.addEventListener('click',function(evt) {
+    evt.preventDefault();
+
+    eraseCanvas();
+    createCanvas();
 });
 
-let color = $("#colorPicker");
-
-//To make the grid pattern makegrid is called whenever button is clicked.
-function makeGrid() {
-
-    c.innerHTML = '';
-    let height = temph.val();
-    let width = tempw.val();
-
-    //A function which fills color in the cell that was clicked and changes the color.
-    let addEvent = function(cell) {
-        cell.addEventListener('click', function() {
-            cell.style.backgroundColor = color.val();
-        });
+function eraseCanvas(){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.firstElementChild);
     }
+}
 
-    //An event listener such that whenever any cell is clicked it calls addEvent function and changes it's color.
-    for (let i = 0; i < height; i++) {
-        let row = c.insertRow(i);
-        for (let j = 0; j < width; j++) {
-            let cell = row.insertCell(j);
-            cell.addEventListener('click', addEvent(cell));
+function createCanvas(){
+    let row = document.getElementById('inputHeight').value,
+        col = document.getElementById('inputWidth').value;
+
+    for (let i = 0; i < row; i++) {
+        let tr = canvas.insertRow(i);                   // inserts row
+
+        for (let j = 0; j < col; j++) {
+            let tc = tr.insertCell(j);                  // inserts cells
         }
     }
 }
+
+canvas.addEventListener('click',function(evt){                          // adding listener on parent for better performance
+    if(evt.target.nodeName == 'TD'){
+        let color = document.getElementById('colorPicker').value;
+        evt.target.style.backgroundColor = color;
+    }
+});
